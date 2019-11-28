3b672038d26c1576be04084bffd16da43d0f28b4
Isac Kim <jk724814@gmail.com>
2019-11-28 17:53:07 +0100

    completed the models and their interactions

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,24 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    # @doses = @cocktail.doses
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params.require(:cocktail).permit(:name))
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+end
